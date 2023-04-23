c30e5d504b5b2835aa51eda1ae450f4cebbdda13
subhdeep paul <subhdeep.paul@sbinfowaves.com>
2023-04-23 16:31:57 +0530

    add svg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 
 import * as tf from '@tensorflow/tfjs';
 import { useEffect, useRef, useState } from 'react';
-
+const circle = "/circle.png";
 export default function Home() {
 
   // // Define a model for linear regression.
@@ -22,6 +22,7 @@ export default function Home() {
   // });
   const [points, setpoints] = useState([])
   const imgRef = useRef(null)
+  const circleRef = useRef(null)
   const canvRef = useRef(null)
 
   const MODEL_PATH = 'https://tfhub.dev/google/tfjs-model/movenet/singlepose/lightning/4'
@@ -42,7 +43,7 @@ export default function Home() {
       let cropSize = [345, 345, 3]
       let cropTensor = tf.slice(imageTensor, cropStartPoint, cropSize)
 
-      let resizeTensor = tf.image.resizeBilinear(imageTensor, [192, 192], true).toInt()
+      let resizeTensor = tf.image.resizeBilinear(cropTensor, [192, 192], true).toInt()
       console.log('---resize tensor---', resizeTensor.shape);
 
       let tensorOutput = moveNet.predict(tf.expandDims(resizeTensor))
@@ -54,53 +55,44 @@ export default function Home() {
     }
   }
 
-  // useEffect(() => {
-  //   (async () => { await loadAndRunModel(imgRef.current) })()
-  // }, [imgRef])
+  useEffect(() => {
+    (async () => { await loadAndRunModel(imgRef.current) })()
+  }, [imgRef])
 
-  // useEffect(() => {
-  //   if (canvRef && imgRef) {
-  //     const c = canvRef.current
-  //     const img = imgRef.current
-  //     var ctx = c.getContext("2d");
-  //     ctx.drawImage(img,10,10);
-  //     console.log('---canvRef---',ctx,img);
+  useEffect(() => {
+    if (canvRef && imgRef && circleRef) {
+      const c = canvRef.current
+      const img = imgRef.current
+      const circle = circleRef.current
+      var ctx = c.getContext("2d");
+      ctx.drawImage(img, 0, 0, img.width, img.height,);
+      if (points.length > 0) {
+        points.forEach(p => {
+          const x = p[1] * 192
+          const y = p[0] * 192
+          ctx.drawImage(circle, x, y, circle.width, circle.height,);
+  
+        })
+        
+      }
+      // console.log('---canvRef---', ctx, img);
 
-  //  }
-  // }, [canvRef,imgRef])
+   }
+  }, [canvRef,imgRef, circleRef, points.length])
 
-
-  const handelPlot = () => {
-    const c = canvRef.current
-    const img = imgRef.current
-    var ctx = c.getContext("2d");
-    ctx.drawImage(img, 0, 0,img.width, img.height,);
-    console.log('---canvRef---', ctx, img);
-  }
+  
 
   return (
     <>
       <div style={{}}>
 
-        {/* <img ref={imgRef} width={640} height={360}  crossOrigin src='https://storage.googleapis.com/jmstore/TensorFlowJS/Edx/standing.jpg' /> */}
         <img ref={imgRef} width={640} height={360} crossOrigin='anonymous'
           // src='https://images.unsplash.com/photo-1470468969717-61d5d54fd036?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=744&q=80'
           src='https://images.unsplash.com/photo-1517438984742-1262db08379e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=680&q=80'
         />
+        <img ref={circleRef} src={circle} width={20} height={20} />
         <canvas ref={canvRef} id="myCanvas" width={640} height={360} style={{ border: '1px solid #000' }}></canvas>
-        <button onClick={handelPlot}>try</button>
-        {/* {points.length > 0 && (
-          <>
-            {points.map((p, i) => {
-              const x = p[1] * 192
-              const y = p[0] * 192
-              return (
-                <div key={i} style={{ position: 'absolute', left: x, top: y, height: 10, width: 10, backgroundColor: 'red', borderRadius: '50%' }} />
-
-              )
-            })}
-          </>
-        )} */}
+      
       </div>
     </>
   )
